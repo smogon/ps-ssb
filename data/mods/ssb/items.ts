da4f6386a1eb1072550279f389d104aec81770fa
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-02-12 01:55:22 -0700

    Add Aeonic (#1)

--- data/mods/ssb/items.ts
+++ data/mods/ssb/items.ts
@@ -1,2 +1,12 @@
 export const Items: {[k: string]: ModdedItemData} = {
+	// Aeonic
+	fossilizedfish: {
+		inherit: true,
+		onResidualOrder: 5,
+		onResidualSubOrder: 4,
+		onResidual(pokemon) {
+			this.heal(pokemon.baseMaxhp / 16);
+		},
+		desc: "At the end of every turn, holder restores 1/16 of its max HP.",
+	},
 };
