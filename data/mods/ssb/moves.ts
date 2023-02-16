fcb9f2efe1f98e6923f26726f88b247bb5018b28
TomOfTomKat <73961857+TomOfTomKat@users.noreply.github.com>
2023-02-16 16:39:41 -0600

    fix formatting

--- data/mods/ssb/moves.ts
+++ data/mods/ssb/moves.ts
@@ -395,7 +395,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		target: "self",
 		type: "Electric",
 	},
-	//phoopes
+	// phoopes
 	gen1blizzard: {
 		accuracy: 90,
 		basePower: 120,
@@ -416,7 +416,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		target: "normal",
 		type: "Ice",
 	},
-	//Scotteh
+	// Scotteh
 	purification: {
 		accuracy: true,
 		basePower: 0,
