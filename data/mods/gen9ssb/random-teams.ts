f2c88cde0ddea5ee5e9819b3ce2242e85763d5f6
Alexander B. <4866817+MathyFurret@users.noreply.github.com>
2023-09-02 20:38:38 -0500

    Add Valerian

--- data/mods/gen9ssb/random-teams.ts
+++ data/mods/gen9ssb/random-teams.ts
@@ -353,6 +353,12 @@ export const ssbSets: SSBSets = {
 		signatureMove: 'Wingover',
 		evs: {hp: 4, atk: 252, spe: 252}, nature: 'Adamant', teraType: 'Flying',
 	},
+	'Valerian ✿ ♡': {
+		species: 'Lucario', ability: 'Full Bloom', item: 'Clear Amulet', gender: 'F',
+		moves: ['Bullet Punch', 'Mach Punch', 'Parting Shot'],
+		signatureMove: 'First Strike',
+		evs: {hp: 252, atk: 252, def: 4}, nature: 'Adamant', teraType: 'Fighting',
+	},
 	Venous: {
 		species: 'Mantine', ability: 'Concrete Over Water', item: 'Leftovers', gender: '',
 		moves: ['Scald', 'Roost', 'Clear Smog'],
