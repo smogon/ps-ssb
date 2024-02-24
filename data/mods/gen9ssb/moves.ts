fc2c8b0214064674fe6140f3ee5db4bc74bbf7ea
WarriorGallade <100499523+WarriorGallade@users.noreply.github.com>
2024-02-24 15:18:53 +0530

    Add Lets go shuckles

--- data/mods/gen9ssb/moves.ts
+++ data/mods/gen9ssb/moves.ts
@@ -1722,6 +1722,28 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		type: "Electric",
 	},
 
+	// Lets go shuckles
+	shucklepower: {
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		pseudoWeather: 'trickroom',
+		volatileStatus: 'powertrick',
+		shortDesc: "Trick Room + Power Trick.",
+		name: "Shuckle Power",
+		pp: 5,
+		priority: -6,
+		flags: {},
+		onTryMove() {
+			this.attrLastMove('[still]');
+		},
+		onPrepareHit(target, source) {
+			this.add('-anim', source, 'Power Trick', source);
+		},
+		target: "self",
+		type: "Psychic",
+	},
+
 	// Lily
 	recharge: {
 		accuracy: true,
