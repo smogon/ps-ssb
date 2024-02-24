d5aa36deda52b070788d8be78d12bfc868feff61
WarriorGallade <100499523+WarriorGallade@users.noreply.github.com>
2024-02-24 11:00:52 +0530

    Add Valerian

--- data/mods/gen9ssb/abilities.ts
+++ data/mods/gen9ssb/abilities.ts
@@ -1528,6 +1528,15 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		flags: {},
 	},
 
+	// Valerian
+	fullbloom: {
+		shortDesc: "This Pokémon's priority moves have double power.",
+		name: "Full Bloom",
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.priority > 0) this.chainModify(2);
+		},
+	},
+
 	// Venous
 	concreteoverwater: {
 		shortDesc: "+1 Def/Spd before getting hit by a Super Effective move",
