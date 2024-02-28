1c943da9b1443856b193c4071a7b0409d251c256
WarriorGallade <100499523+WarriorGallade@users.noreply.github.com>
2024-02-28 21:33:43 +0530

    Update messages

--- data/mods/gen9ssb/conditions.ts
+++ data/mods/gen9ssb/conditions.ts
@@ -212,13 +212,14 @@ export const Conditions: {[k: string]: ModdedConditionData & {innateName?: strin
 		}, // Is a message before going mega supported(unlikely)? if so, the first message in onAfterMega goes there.
 	},
 	arcueid: {
-		// TBA
 		noCopy: true,
 		onStart() {
-		},
-		onSwitchOut() {
+			const img = "https://i.ibb.co/Cv8LYmP/the-rock-look-the-rock-meme.gif";
+			this.add(`c:|${getName('Arcueid')}|/html <img src="${img}" width="32" height="32" />`);
 		},
 		onFaint() {
+			const img = "https://i.ibb.co/ss0VF8x/tRsWWx7.png";
+			this.add(`c:|${getName('Arcueid')}|/html <img src="${img}" width="32" height="32" />`);
 		},
 	},
 	arsenal: {
