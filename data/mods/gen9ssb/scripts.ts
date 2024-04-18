095b68ed01459ed4d8e8ecd374db9a07e48ad420
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2024-04-18 15:32:54 -0600

    Update data/mods/gen9ssb/scripts.ts

--- data/mods/gen9ssb/scripts.ts
+++ data/mods/gen9ssb/scripts.ts
@@ -1088,7 +1088,7 @@ export const Scripts: ModdedBattleScriptsData = {
 				const dancers = [];
 				for (const currentPoke of this.battle.getAllActive()) {
 					if (pokemon === currentPoke) continue;
-					if ((currentPoke.hasAbility('dancer') || currentPoke.hasAbility('virtualidol')) && !currentPoke.isSemiInvulnerable()) {
+					if (currentPoke.hasAbility(['dancer', 'virtualidol']) && !currentPoke.isSemiInvulnerable()) {
 						dancers.push(currentPoke);
 					}
 				}
